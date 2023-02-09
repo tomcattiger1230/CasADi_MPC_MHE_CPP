347f2bf56c88c9ae834373d2dafb7d818e065bc5
Wei Luo <wei.luo.88@outlook.com>
2023-02-09 20:58:53 +0100

    update

--- MPC/quadrotor/src/dmoc_quadrotor_controller.cpp
+++ MPC/quadrotor/src/dmoc_quadrotor_controller.cpp
@@ -2,7 +2,7 @@
  * @Author: Wei Luo
  * @Date: 2023-01-05 22:35:40
  * @LastEditors: Wei Luo
- * @LastEditTime: 2023-02-09 17:52:24
+ * @LastEditTime: 2023-02-09 19:12:09
  * @Note: Note
  */
 
@@ -81,14 +81,11 @@ int main() {
   init_trajectory.block<12, 1>(0, 19) << 1.0, 0.2, 1.0, 0, 0, 0, 0, 0, 0, 0, 0,
       0;
 
-  const double hover_force =
-      g_acceleration * quadrotor_mass * 0.25;
+  const double hover_force = g_acceleration * quadrotor_mass * 0.25;
   std::vector<double> control_lb = {hover_force * 0.5, hover_force * 0.5,
-                                    hover_force * 0.5,
-                                    hover_force * 0.5};
+                                    hover_force * 0.5, hover_force * 0.5};
   std::vector<double> control_ub = {hover_force * 1.5, hover_force * 1.5,
-                                    hover_force * 1.5,
-                                    hover_force * 1.5};
+                                    hover_force * 1.5, hover_force * 1.5};
   std::vector<double> state_lb = {-ca::inf, -ca::inf, 0.0, -M_PI, -M_PI, -M_PI};
   std::vector<double> state_ub = {ca::inf, ca::inf, ca::inf, M_PI, M_PI, M_PI};
   std::vector<double> v_limit{ca::inf, ca::inf, ca::inf};
@@ -107,8 +104,7 @@ int main() {
   real_trajectory_std.push_back(current_state);
   std::vector<double> parameter_std;
 
-  while (iter_index < max_iter)
-  {
+  while (iter_index < max_iter) {
     ref_trajectory.block<12, 1>(0, 0) = current_state;
     auto start_time = std::chrono::high_resolution_clock::now();
     std::vector<double> init_state_std(opt_x.data(),
@@ -125,18 +121,18 @@ int main() {
     //     current_state.data(), current_state.data() + current_state.size());
     std::vector<double> ref_trajectory_std(
         ref_trajectory.data(), ref_trajectory.data() + ref_trajectory.size());
-    // std::cout << "reference trajectory" << ref_trajectory.transpose() << std::endl;
-                     // std::merge(current_state.begin(), current_state.end(),
-                     //            ref_trajectory_std.begin(),
-                     //            ref_trajectory_std.end(),
-                     //            std::back_inserter(parameter_std));
-                         dmoc_uav_handle->get_results(
-                             opt_init, ref_trajectory_std, opt_x, opt_u);
+    // std::cout << "reference trajectory" << ref_trajectory.transpose() <<
+    // std::endl; std::merge(current_state.begin(), current_state.end(),
+    //            ref_trajectory_std.begin(),
+    //            ref_trajectory_std.end(),
+    //            std::back_inserter(parameter_std));
+    dmoc_uav_handle->get_results(opt_init, ref_trajectory_std, opt_x, opt_u);
     // std::cout << opt_x.transpose() << std::endl;
     // std::cout << opt_u.transpose() << std::endl;
     dmoc_uav_handle->model_based_movement(current_state, opt_u.col(0), opt_x,
                                           opt_u);
-    // std::cout << "robot's current state: " << current_state.transpose() << std::endl;
+    // std::cout << "robot's current state: " << current_state.transpose() <<
+    // std::endl;
     auto stop_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
         stop_time - start_time);
